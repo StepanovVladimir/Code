edc93e3c8ebec1ef63ea4fe0233094af94e4e9ad
Vladimir Stepanov <vovan.number.one@mail.ru>
2017-11-13 10:39:14 +0400

    global constants

--- workshop2/workshop2.1/main.cpp
+++ workshop2/workshop2.1/main.cpp
@@ -1,13 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+constexpr unsigned WINDOW_WIDTH = 800;
+constexpr unsigned WINDOW_HEIGHT = 600;
+constexpr unsigned BALL_RADIUS = 40;
+
 struct Ball
 {
     sf::CircleShape shape;
-    sf::Vector2f position;
-    float size;
     sf::Vector2f speed;
-    sf::Color color;
 };
 
 void init(std::vector<Ball> &balls)
@@ -22,7 +23,6 @@ void init(std::vector<Ball> &balls)
         {-150.f, 150.f},
         {200.f, -200.f},
         {-250.f, -250.f}};
-    const std::vector<float> sizes = {40, 40, 40, 40};
     const std::vector<sf::Vector2f> positions = {
         {80, 80},
         {600, 80},
@@ -30,14 +30,11 @@ void init(std::vector<Ball> &balls)
         {500, 450}};
     for (size_t i = 0; i < 4; ++i)
     {
-        balls[i].color = colors[i];
-        balls[i].size = sizes[i];
         balls[i].speed = speeds[i];
-        balls[i].position = positions[i];
-        balls[i].shape.setPosition(balls[i].position);
-        balls[i].shape.setOrigin(balls[i].size, balls[i].size);
-        balls[i].shape.setRadius(balls[i].size);
-        balls[i].shape.setFillColor(balls[i].color);
+        balls[i].shape.setPosition(positions[i]);
+        balls[i].shape.setOrigin(BALL_RADIUS, BALL_RADIUS);
+        balls[i].shape.setRadius(BALL_RADIUS);
+        balls[i].shape.setFillColor(colors[i]);
     }
 }
 
@@ -64,19 +61,19 @@ void update(std::vector<Ball> &balls, float &deltaTime)
         sf::Vector2f position = balls[i].shape.getPosition();
         position += balls[i].speed * deltaTime;
 
-        if ((position.x + balls[i].size >= 800) && (balls[i].speed.x > 0))
+        if ((position.x + BALL_RADIUS >= WINDOW_WIDTH) && (balls[i].speed.x > 0))
         {
             balls[i].speed.x = -balls[i].speed.x;
         }
-        if ((position.x - balls[i].size < 0) && (balls[i].speed.x < 0))
+        if ((position.x - BALL_RADIUS < 0) && (balls[i].speed.x < 0))
         {
             balls[i].speed.x = -balls[i].speed.x;
         }
-        if ((position.y + balls[i].size >= 600) && (balls[i].speed.y > 0))
+        if ((position.y + BALL_RADIUS >= WINDOW_HEIGHT) && (balls[i].speed.y > 0))
         {
             balls[i].speed.y = -balls[i].speed.y;
         }
-        if ((position.y - balls[i].size < 0) && (balls[i].speed.y < 0))
+        if ((position.y - BALL_RADIUS < 0) && (balls[i].speed.y < 0))
         {
             balls[i].speed.y = -balls[i].speed.y;
         }
@@ -99,7 +96,7 @@ int main()
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(
-        sf::VideoMode({800, 600}),
+        sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
         "Balls", sf::Style::Default, settings);
     sf::Clock clock;
     std::vector<Ball> balls(4);
